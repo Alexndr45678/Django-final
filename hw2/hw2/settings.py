5b2d2f4afe97e04bd9b2a72fb3046f860868586c
Alexandr <111227400+Alexndr45678@users.noreply.github.com>
2024-01-31 14:41:49 +0300

    update

--- hw2/hw2/settings.py
+++ hw2/hw2/settings.py
@@ -21,15 +21,25 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-(yt@9a!wl_9(e0$sm7sip)(x3konmj&6j17g703ooc@=^e@i#3"
+# SECRET_KEY = "django-insecure-(yt@9a!wl_9(e0$sm7sip)(x3konmj&6j17g703ooc@=^e@i#3"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "asdw.pythonanywhere.com",
+]
 
 
 # Application definition
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -39,9 +49,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "hw2_app",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -77,8 +89,15 @@ WSGI_APPLICATION = "hw2.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "<asdw>$<asdwe$default>",
+        "USER": "<asdw>",
+        "PASSWORD": os.getenv("MYSQL_PASSWORD"),
+        "HOST": "<your_mysql_hostname>",
+        "OPTIONS": {
+            "init_command": "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+            "charset": "utf8mb4",
+        },
     }
 }
 
